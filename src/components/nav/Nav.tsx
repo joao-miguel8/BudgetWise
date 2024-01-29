809d4e520c4ae6aeb52e8a252f1c93ccda5fa39a
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2024-01-29 12:25:54 -0500

    - Add close menu button for Nav.tsx

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,6 +2,7 @@ import { GrTransaction } from "react-icons/gr";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { PiTarget } from "react-icons/pi";
 import { IoCardOutline } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 import type { NavItemType } from "./types/NavItemType";
 
 function Nav() {
@@ -13,8 +14,11 @@ function Nav() {
 	];
 
 	return (
-		<div className="h-screen w-full min-[450px]:w-[240px] z-[60] fixed top-0 bg-[rgb(249,249,249)] ">
-			<nav className="mt-10">
+		<div className="h-screen w-full min-[450px]:w-[240px] z-[60] fixed top-0 bg-[rgb(249,249,249)]">
+			<nav className="mt-16">
+				<button className="absolute top-2 left-4">
+					<IoMdClose size={"1.6rem"} />
+				</button>
 				<ul className="flex flex-col gap-4">
 					{navItems.map((item: NavItemType) => {
 						return (
